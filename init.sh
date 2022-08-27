5c7e71702a95db851462728dbd00d0d02fc85574
orvice L <orvice@gmail.com>
2022-08-27 20:16:43 +0800

    Init

---
+++ init.sh
@@ -0,0 +1,52 @@
+
+echo "Init"
+apt-get update 
+apt-get install -y git curl vim vnstat tmux nscd  dnsutils sudo net-tools zsh gnupg2 wget
+
+echo "set iptables"
+update-alternatives --config iptables
+
+echo "Installing zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+
+
+echo "setting bbr"
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+
+
+sudo sysctl -p
+sudo sysctl net.ipv4.tcp_available_congestion_control
+sudo lsmod | grep bbr
+
+
+echo "Install docker"
+
+echo "Install dependence"
+sudo apt-get update
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+echo "Add Docker’s official GPG key"
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+
+echo "Install docker"
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+   
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+
+apt-cache madison docker-ce
+
+echo "Install docker-compose"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
